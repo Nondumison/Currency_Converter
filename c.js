a03bcb732b2909bd98388dcdfc8f3c85114dd895
Nondumison <nkosindumi77@gmail.com>
2018-07-06 10:22:00 +0200

    currency_converter

--- c.js
+++ c.js
@@ -2,22 +2,24 @@ function convertCurrency()
 
 {
 
-var from = document.getElementById("from").value;
-var to = document.getElementById("to").value;
-var xmlhttp = new XMLHttpRequest();
-var url ="https://openexchangerates.org/api/latest.json?app_id=730a54266d924f719c7bafc7ca2952c7";
+let from = document.getElementById("from").value;
+let to = document.getElementById("to").value;
+let xmlhttp = new XMLHttpRequest();
+const url ="https://openexchangerates.org/api/latest.json?app_id=730a54266d924f719c7bafc7ca2952c7";
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 xmlhttp.onreadystatechange = function() {
 if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
 {
-var result = xmlhttp.responseText;
-var jsResult = JSON.parse(result);
-var oneUnit = jsResult.rates[to]/jsResult.rates[from];
-var amt = document.getElementById("fromAmount").value;
+let result = xmlhttp.responseText;
+let jsResult = JSON.parse(result);
+let oneUnit = jsResult.rates[to]/jsResult.rates[from];
+let amt = document.getElementById("fromAmount").value;
 document.getElementById("toAmount").value = (oneUnit*amt).toFixed(2);
 }
 
 
 }
+
+
 }
